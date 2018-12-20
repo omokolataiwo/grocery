e6413306636c5188654fe17a2d2503025958e629
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2018-12-20 20:24:31 +0100

    server frontend from backend

--- server/index.js
+++ server/index.js
@@ -2,12 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import path from 'path';
 import baseRoute from './router';
 import enVariable from './envConfig';
 
 const { dbConnection } = enVariable;
-
-console.log(dbConnection);
 
 mongoose.connect(
   dbConnection,
@@ -17,10 +16,13 @@ mongoose.connect(
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('./web/public/'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/v1', baseRoute);
 
-app.listen(process.env.PORT, () => console.log('Server started on port: ', process.env.PORT));
+app.use('*', (req, res) => res.status(200).sendFile(path.join(__dirname, '../web/public/index.html')));
+
+app.listen(process.env.PORT || 7000);
 
 export default app;
